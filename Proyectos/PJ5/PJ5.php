80a973128cbd1349a41e9c4ff50416fec014ed19
Alexis Escalona <alexisescalona2@hotmail.com>
2019-08-31 16:44:01 -0500

    En proceso

---
+++ Proyectos/PJ5/PJ5.php
@@ -0,0 +1,293 @@
+<div class="tab-pane fade" id="JuegoEducativo" role="tabpanel">
+	<div class="card">
+		<div class="card-body">
+			<ul class="nav md-pills nav-justified rounded-lg mb-0 border border-dark elegant-color pills-warning">
+				<li class="nav-item">
+					<a class="nav-link active" data-toggle="tab" href="#PJ5VG" role="tab"><i class="fas fa-binoculars"></i> Visión General</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="tab" href="#PJ5C" role="tab"><i class="fas fa-list-alt"></i> Características</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="tab" href="#PJ5DT" role="tab"><i class="fas fa-clipboard-list"></i> Detalles Técnicos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="tab" href="#PJ5I" role="tab"><i class="far fa-images"></i> Capturas</a>
+				</li>
+			</ul>
+			<div class="tab-content pt-3">
+				<div class="tab-pane fade in show active" id="PJ5VG" role="tabpanel">
+					<div class="card white text-dark">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-md-8">
+									<b>¿Qué es?</b>
+									<br><p>Programa dedicado a enseñar a los niños sumas, restas, multiplicaciones y divisiones.</p>
+									<b>Objetivo</b>
+									<br><p class="mb-0">Fortalecer el aprendizaje de los niños que a través de un juego
+									dependiendo de la dificultad los obligue a superarse a ellos mismos.</p>
+								</div>
+								<div class="col-md-4">
+									<div class="mdb-lightbox no-margin">
+										<figure class="col">
+											<a href="Proyectos/PJ5/Images/Interfaz.jpg" data-size="609x677">
+												<img src="Proyectos/PJ5/Images/Interfaz.jpg" class="img-fluid" style="width:246px; height:186px;">
+											</a>
+											<figcaption>
+												<b>Interfaz principal de la aplicación(Menú de Selección) <i class="fas fa-desktop"></i></b>
+											</figcaption>
+										</figure>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="PJ5C" role="tabpanel">
+					<div class="card white text-dark">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="table-responsive text-nowrap">
+										<table class="table table-sm">
+											<tbody class="text-center">
+												<tr>
+													<td class="p-0 font-weight-bold text-left" colspan="4"><p class="mt-1 mb-0">El juego tiene 4 modos de juego:</td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0">Suma</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Resta</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Multiplicación</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">División</p></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+									<div class="table-responsive text-nowrap">
+										<table class="table table-sm">
+											<tbody class="text-center">
+												<tr>
+													<td class="p-0 font-weight-bold text-left" colspan="3"><p class="mt-1 mb-0">Consta de 3 dificultades para cada modalidad:</td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0">Fácil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Medio</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Difícil</p></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+									<div class="table-responsive text-nowrap">
+										<table class="table table-sm m-0">
+											<tbody class="text-center">
+												<tr>
+													<td class="p-0 font-weight-bold text-left" colspan="4"><p class="mt-1 mb-0">Características de acuerdo al modo y dificultad seleccionada:</td>
+												</tr>
+												<tr>
+													<td class="p-0"></td>
+													<td class="p-0"></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0"># Dígitos</p></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Temporizador</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal" rowspan="3" style="vertical-align:middle"><p class="mt-1 mb-0">Suma</p></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Fácil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">S/T</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Medio</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 2</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">20 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Difícil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 3</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">10 segundos</p></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="table-responsive text-nowrap">
+										<table class="table table-sm m-0">
+											<tbody class="text-center">
+												<tr>
+													<td class="p-0 font-weight-normal" rowspan="3" style="vertical-align:middle"><p class="mt-1 mb-0">Resta</p></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Fácil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">S/T</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Medio</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 2</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">20 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Difícil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 3</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">10 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal" rowspan="3" style="vertical-align:middle"><p class="mt-1 mb-0">Multiplicación</p></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Fácil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">S/T</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Medio</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 2</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">20 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Difícil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 2</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">10 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal" rowspan="3" style="vertical-align:middle"><p class="mt-1 mb-0">División</p></td>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Fácil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">S/T</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Medio</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 3</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">20 segundos</p></td>
+												</tr>
+												<tr>
+													<td class="p-0 font-weight-normal"><p class="mt-1 mb-0">Difícil</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">1 a 3</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">10 segundos</p></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="PJ5DT" role="tabpanel">
+					<div class="card white text-dark">
+						<div class="card-body">
+							<div class="row">
+								<div class="col">
+									<div class="table-responsive text-nowrap">
+										<table class="table table-sm table-bordered m-0">
+											<tbody>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">Tecnologías</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">JDK 8 y Swing</p></td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">Librerías adicionales</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">S/L</p></td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">Plataforma</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Aplicación de escritorio</p></td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">Sistema Operativo</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">Windows</p></td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">Memoria en disco requerida</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">7.57 mb</p></td>
+												</tr>
+												<tr>
+													<td class="p-0"><p class="mt-1 mb-0 font-weight-bold">RAM requerida</p></td>
+													<td class="p-0"><p class="mt-1 mb-0">200 mb</p></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="PJ5I" role="tabpanel">
+					<div class="card white text-dark">
+						<div class="card-body">
+							<div class="col">
+								<div id="carruselPJ5" class="carousel slide carousel-multi-item mb-0" data-ride="carousel" data-interval="false">
+									<div class="carousel-inner">
+										<div class="controls-top mb-0">
+											<a class="btn-floating btn-warning" href="#carruselPJ5" data-slide="prev"><i
+											class="fas fa-chevron-left"></i></a>
+											<a class="btn-floating btn-warning" href="#carruselPJ5" data-slide="next"><i
+											class="fas fa-chevron-right"></i></a>
+										</div>
+										<div class="carousel-item active">
+											<div class="row justify-content-center m-0">
+												<div class="mdb-lightbox">
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-1.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-1.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>Primera interfaz: Selección de modo de juego.</b>
+														</figcaption>
+													</figure>
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-2.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-2.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>Segunda interfaz: Selección de dificultad.</b>
+														</figcaption>
+													</figure>
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-3.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-3.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>Tercera interfaz: Empieza el juego.</b>
+														</figcaption>
+													</figure>
+												</div>
+											</div>
+										</div>
+										<div class="carousel-item">
+											<div class="row justify-content-center m-0">
+												<div class="mdb-lightbox">
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-4.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-4.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>En caso de haber perdido y superar algún récord aparece pantalla de guardar récord.</b>
+														</figcaption>
+													</figure>
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-5.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-5.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>En caso de haber perdido y no superar algún récord aparece pantalla de volver a intentarlo.</b>
+														</figcaption>
+													</figure>
+													<figure class="col-md-4">
+														<a href="Proyectos/PJ5/Images/PJ5-6.jpg" data-size="609x677">
+															<img src="Proyectos/PJ5/Images/PJ5-6.jpg" class="img-fluid" style="width:246px; height:186px;">
+														</a>
+														<figcaption>
+															<b>Cuarta interfaz: Visualización de récords establecidos.</b>
+														</figcaption>
+													</figure>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
